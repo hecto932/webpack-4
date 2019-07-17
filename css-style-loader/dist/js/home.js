7d8a3db8d5b5d32a78d918c5e7e8c356bdc99ff8
Hector Flores <hflores@nearsoft.comm>
2019-07-16 17:18:12 -0700

    using plugins

--- css-style-loader/dist/js/home.js
+++ css-style-loader/dist/js/home.js
@@ -116,7 +116,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\")(false);\n// Module\nexports.push([module.i, \"body {\\n  background-color: lightblue;\\n  display: flex;\\n  align-content: center;\\n  justify-content: center;\\n  min-height: 100vh;\\n  margin: 0;\\n  font-size: 40px;\\n  font-family: Arial, Helvetica, sans-serif;\\n}\", \"\"]);\n\n\n//# sourceURL=webpack:///./css-style-loader/src/css/index.css?./node_modules/css-loader/dist/cjs.js");
+eval("exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\")(false);\n// Module\nexports.push([module.i, \"body {\\n  background-color: lightblue;\\n  display: flex;\\n  align-items: center;\\n  justify-content: center;\\n  min-height: 100vh;\\n  margin: 0;\\n  font-size: 40px;\\n  font-family: Arial, Helvetica, sans-serif;\\n}\", \"\"]);\n\n\n//# sourceURL=webpack:///./css-style-loader/src/css/index.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
